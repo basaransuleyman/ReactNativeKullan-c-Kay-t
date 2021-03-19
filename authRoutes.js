7e5cd37b23bb837aa7043a284f738a72376de73a
Süleyman <44279036+basaransuleyman@users.noreply.github.com>
2021-03-20 02:12:05 +0300

    Update authRoutes.js

--- authRoutes.js
+++ authRoutes.js
@@ -25,30 +25,4 @@ router.post('/signup',async (req,res)=>{
 
 
 
-
-
-router.post('/signin',async (req,res)=>{
-    const {email,password} = req.body
-    if(!email || !password){
-        return res.status(422).send({error :"must provide email or password"})
-    }
-    const user = await User.findOne({email}) //emaile göre user bulma 
-    
-    if(!user){
-        return res.status(422).send({error :"must provide email or password"})
-    }
-    try{
-      await user.comparePassword(password);    
-      const token = jwt.sign({userId:user._id},jwtkey)
-      res.send({token})
-    }catch(err)
-    {
-        return res.status(422).send({error :"must provide email or password"})
-    }
-    
-
-
-})
-
-
 module.exports = router
